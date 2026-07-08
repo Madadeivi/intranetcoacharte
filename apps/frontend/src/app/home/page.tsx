16a703bc2f7e7ae74e308cac14ec1306ac58b9f3
A Mohedano <servicios@coacharte.mx>
2026-07-08 15:13:31 -0600

    Corrección

--- apps/frontend/src/app/home/page.tsx
+++ apps/frontend/src/app/home/page.tsx
@@ -523,8 +523,8 @@ const HomePage: React.FC = () => {
       <footer className="home-footer">
         <div className="footer-content">
           <div className="footer-brand-section">
-            <Image src="/assets/logo_coacharte1.png" alt="Coacharte Blanco" className="footer-logo" width={180} height={45} />
-            <p className="footer-slogan">Inspirados para transformar cualquier reto en logros</p>
+            <Image src="/assets/coacharte-bco@4x.png" alt="Coacharte Blanco" className="footer-logo" width={180} height={45} />
+            <p className="footer-slogan">Transformamos cualquier reto en logro</p>
           </div>
           <div className="footer-links-section">
             <div className="footer-col">
